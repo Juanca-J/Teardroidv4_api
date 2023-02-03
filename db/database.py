075b6891910867805a126abb4a764e335d6f7454
Juanca_J <106214770+Juanca-J@users.noreply.github.com>
2023-02-03 17:28:56 -0500

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,7 +1,7 @@
 from deta import Deta
 from os import getenv
 
-deta = Deta("e088wg5t_kfqwuU5c2rADLnqxxzUWZ1ahNFUrLqk9")
+deta = Deta("e0iwribv_7zX1sVjDtoLy3sizHDx3JyEQnjg7QUic")
 
 
 def client_db():
